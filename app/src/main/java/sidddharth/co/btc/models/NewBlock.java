7024d46aaedbffc315aa17efa68fa2d56e8661f0
Siddharth <siddharth.shah@elanic.in>
2019-04-27 09:15:23 +0530

    pojo for newblock

--- app/src/main/java/sidddharth/co/btc/models/NewBlock.java
+++ app/src/main/java/sidddharth/co/btc/models/NewBlock.java
@@ -1,4 +1,50 @@
 package sidddharth.co.btc.models;
 
 public class NewBlock {
+    String hash;
+    double totalBTCSent;
+    double reward;
+    double size;
+    int height;
+
+    public String getHash() {
+        return hash;
+    }
+
+    public double getTotalBTCSent() {
+        return totalBTCSent;
+    }
+
+    public double getReward() {
+        return reward;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public void setTotalBTCSent(double totalBTCSent) {
+        this.totalBTCSent = totalBTCSent;
+    }
+
+    public void setReward(double reward) {
+        this.reward = reward;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
