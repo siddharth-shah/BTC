39b5f8b39b5de128ed0fdb7f716f297b86db92cb
Siddharth <siddharth.shah@elanic.in>
2019-04-27 09:23:44 +0530

    setup for setting newBlock info (parsing json and setting values)

--- app/src/main/java/sidddharth/co/btc/MainActivity.java
+++ app/src/main/java/sidddharth/co/btc/MainActivity.java
@@ -16,6 +16,7 @@ import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
+import sidddharth.co.btc.models.NewBlock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +24,19 @@ public class MainActivity extends AppCompatActivity {
     TextView webSocketConnectionStatusText;
     private static final String OP_UTX = "utx";
     private static final String OP_BLOCK = "block";
+    TextView blockHash;
+    TextView btcSent;
+    TextView blockSize;
+    TextView reward;
+    TextView blockHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webSocketConnectionStatusText = findViewById(R.id.ws_connection_status);
+        setupNewBlockInfoUI();
+
         okHttpClient = new OkHttpClient.Builder()
                 .readTimeout(3, TimeUnit.SECONDS)
                 .build();
@@ -68,7 +76,8 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(text);
                     final String messageType = jsonObject.optString("op");
                     if (OP_BLOCK.equalsIgnoreCase(messageType)) {
-                        parseBlock(jsonObject);
+                        final NewBlock newBlock = parseBlock(jsonObject);
+                        setNewBlockInfo(newBlock);
                     } else if (OP_UTX.equalsIgnoreCase(messageType)) {
                         parseUnConfirmedTransaction(jsonObject);
                     }
@@ -94,8 +103,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void parseBlock(JSONObject jsonObject) {
+    private void setupNewBlockInfoUI() {
+        blockHash = findViewById(R.id.block_hash);
+        blockHeight = findViewById(R.id.block_height);
+        reward = findViewById(R.id.reward);
+        btcSent = findViewById(R.id.btc_sent);
+        blockSize = findViewById(R.id.block_size);
+    }
 
+    private void setNewBlockInfo(final NewBlock newBlock) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                blockHash.setText("Block Hash: " + newBlock.getHash());
+                blockHeight.setText("Block Height: " + String.valueOf(newBlock.getHeight()));
+                blockSize.setText("Block size: " + String.valueOf(newBlock.getSize()));
+                reward.setText("Reward: " + String.valueOf(newBlock.getReward()));
+                btcSent.setText("BTC Sent: " + String.valueOf(newBlock.getTotalBTCSent()));
+            }
+        });
+
+    }
+
+    private NewBlock parseBlock(JSONObject jsonObject) {
+        NewBlock newBlock = new NewBlock();
+        newBlock.setHash(jsonObject.optString("hash"));
+        newBlock.setHeight(jsonObject.optInt("height"));
+        newBlock.setReward(jsonObject.optDouble("reward"));
+        newBlock.setSize(jsonObject.optDouble("size"));
+        newBlock.setTotalBTCSent(jsonObject.optDouble("totalBTCSent"));
+        return newBlock;
     }
 
     private void parseUnConfirmedTransaction(JSONObject jsonObject) {
