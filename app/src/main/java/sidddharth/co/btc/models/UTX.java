41bce8ba2fd3b84af00d0a7b4101efc86f56aa56
Siddharth <siddharth.shah@elanic.in>
2019-04-27 09:47:47 +0530

    created pojo for unconfirmed tx

--- app/src/main/java/sidddharth/co/btc/models/UTX.java
+++ app/src/main/java/sidddharth/co/btc/models/UTX.java
@@ -1,4 +1,31 @@
 package sidddharth.co.btc.models;
 
 public class UTX {
+    String transactionHash;
+    double transactionAmnt;
+    long timestamp;
+
+    public String getTransactionHash() {
+        return transactionHash;
+    }
+
+    public void setTransactionHash(String transactionHash) {
+        this.transactionHash = transactionHash;
+    }
+
+    public double getTransactionAmnt() {
+        return transactionAmnt;
+    }
+
+    public void setTransactionAmnt(double transactionAmnt) {
+        this.transactionAmnt = transactionAmnt;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
